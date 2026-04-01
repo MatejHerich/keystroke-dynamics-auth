b12f7ee9b275978795b8c0fecb0b323c72aba9a8
Matej Herich <herich.matej2005@gmail.com>
2026-04-01 16:44:00 +0200

    Update repository interfaces for new entity structure

--- app-backend/src/main/java/com/example/appbackend/VerificationSampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.appbackend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface VerificationSampleRepository extends JpaRepository<VerificationSample, Long> {
-
-}
